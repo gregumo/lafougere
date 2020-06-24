647e951b2d62e1137a15ae6f953de7bd6ab2bbbb
Grégoire Humeau <gregoire@troopers.email>
2020-06-24 23:43:40 +0200

    Git pull during deploy

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ set -e
 docker-compose pull
 
 #Set maintenance mode to perform critical operations (database, upgrades, ...)
-APP_MAINTENANCE=1 docker-compose up -d
-#i.e: docker-compose exec -T app bin/console doctrine:migration:migrate -n
-sleep 10; #for demo purpose
+APP_MAINTENANCE=1 docker-compose down
+#docker-compose exec -T app bin/console doctrine:migration:migrate -n
+git pull
 
 #back to prod
 docker-compose up -d
